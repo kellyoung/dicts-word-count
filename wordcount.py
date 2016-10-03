8f426d725f0ff9c0db3d0f807847556cf424f203
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:45:42 -0700

    Finished initial work

---
+++ wordcount.py
@@ -0,0 +1,14 @@
+# put your code here.
+def word_count(txt_file):
+    count_of_words = {}
+    
+    with open(txt_file) as the_file:
+        words = the_file.read().rstrip().split()
+
+        for word in words:
+            count_of_words[word] = count_of_words.get(word, 0) + 1
+
+        for word, count in count_of_words.iteritems():
+            print word, count
+
+word_count("twain.txt")
